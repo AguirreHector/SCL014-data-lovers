784a9dccde620902d956c950a0f1c5ee030d24fb
Hector Aguirre <taatoaguirre@gmail.com>
2021-03-14 11:22:34 -0300

    working for show the pokemons cards

--- src/data.js
+++ src/data.js
@@ -1,5 +1,16 @@
-// estas funciones son de ejemplo
+/* estas funciones son de ejemplo
 
 export const example = () => 'example';
 
-export const anotherExample = () => 'OMG';
+export const anotherExample = () => 'OMG';*/
+
+
+
+export const filtraNombre = (datos, string) => {
+
+    const arrayNombre = [...datos];
+    const arrayNombreFiltrado = arrayDatos.filter(dato => dato.name
+        .indexOf(string[0].toUpperCase() + string.slice(1).toLowerCase()) > -1)
+
+    return arrayNombreFiltrado;
+}
--- src/main.js
+++ src/main.js
@@ -1,6 +1,27 @@
-import { example } from './data.js';
+// import { example } from './data.js';
 // import data from './data/atletas/atletas.js';
-// import data from './data/lol/lol.js';
-import data from './data/pokemon/pokemon.js';
 
-console.log(example, data);
+import DATOS from './data/pokemon/pokemon.js';
+
+console.log(DATOS);
+// Función que muestra los personajes en Html
+
+const mostrarTodosLosPokemon = (data) => {
+    let plantillaPokemon = '';
+    data.forEach((obj) => {
+        plantillaPokemon += `
+        <div class="tarjeta">
+            <div class="contendorTarjeta">
+                <img src = '${obj.image}' alt = "Image" class="imagenPokemon"/>
+                <h2 class="nombrePokemon">${obj.name}</h2>
+                <li>Tipo: ${obj.type}</li>
+                <li>Fuerte contra: ${obj.resistant}</li>
+                <li>Débil contra: ${obj.weaknesses}</li>
+            </div>
+        </div>`;
+    });
+    return plantillaPokemon;
+  };
+
+const muestra = document.getElementById('muestra')
+muestra.textContent = mostrarTodosLosPokemon(DATOS)
